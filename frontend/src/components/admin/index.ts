5b4041819fd94a4466e497170ee75bfea519e4fe
Akshat Jain <realakshatjain@gmail.com>
2025-10-18 15:04:22 +0530

    add index.ts

---
+++ frontend/src/components/admin/index.ts
@@ -0,0 +1,3 @@
+export { default as CourseApprovalCard } from "./CourseApprovalCard";
+export { default as UserManagementTable } from "./UserManagementTable";
+export { default as PlatformAnalytics } from "./PlatformAnalytics";
