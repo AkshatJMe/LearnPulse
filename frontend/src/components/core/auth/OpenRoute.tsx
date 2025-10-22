cdde623e99864a54b6d808913bfd2da9c275a2a4
Akshat Jain <realakshatjain@gmail.com>
2025-10-22 22:13:59 +0530

    update OpenRoute.tsx

--- frontend/src/components/core/auth/OpenRoute.tsx
+++ frontend/src/components/core/auth/OpenRoute.tsx
@@ -12,7 +12,7 @@ const OpenRoute: React.FC<OpenRouteProps> = ({ children }) => {
   if (token === null) {
     return <>{children}</>;
   } else {
-    return <Navigate to="/dashboard/my-profile" />;
+    return <Navigate to="/dashboard/profile" />;
   }
 };
 
