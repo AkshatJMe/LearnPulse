a817bdb99f4532acd96e5f61d4077006e5af7139
Akshat Jain <realakshatjain@gmail.com>
2025-10-26 16:35:21 +0530

    add index.ts

---
+++ frontend/src/components/course/index.ts
@@ -0,0 +1,8 @@
+export { default as CourseCard } from './CourseCard';
+export { default as CourseReview } from './CourseReview';
+export { default as CourseReviews } from './CourseReviews';
+export { default as Curriculum } from './Curriculum';
+export { default as InstructorCard } from './InstructorCard';
+export { default as RatingBreakdown } from './RatingBreakdown';
+export { default as CourseMeta } from './CourseMeta';
+export { default as CourseOverview } from './CourseOverview';
