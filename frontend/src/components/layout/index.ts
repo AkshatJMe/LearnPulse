4bacec3d01879d2ac712aaf477f4f0df5838f2a8
Akshat Jain <realakshatjain@gmail.com>
2025-11-02 05:00:45 +0530

    add index.ts

---
+++ frontend/src/components/layout/index.ts
@@ -0,0 +1,6 @@
+export { default as Navbar } from './Navbar';
+export { default as Sidebar } from './Sidebar';
+export { default as Footer } from './Footer';
+export { default as MainLayout } from './MainLayout';
+export { default as DashboardLayout } from './DashboardLayout';
+export { default as AuthLayout } from './AuthLayout';
