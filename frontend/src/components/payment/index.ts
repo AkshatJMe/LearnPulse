0712ef589dd2ed193acd2596f123d06bfc4cc6c0
Akshat Jain <realakshatjain@gmail.com>
2025-11-04 11:27:47 +0530

    add index.ts

---
+++ frontend/src/components/payment/index.ts
@@ -0,0 +1,3 @@
+export { default as OrderSummary } from './OrderSummary';
+export { default as StripeCheckoutForm } from './StripeCheckoutForm';
+export type { PaymentDetails } from './StripeCheckoutForm';
