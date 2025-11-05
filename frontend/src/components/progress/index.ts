8177af861e334cb9ed7cabd6544ba6621f736cdf
Akshat Jain <realakshatjain@gmail.com>
2025-11-05 07:22:44 +0530

    add index.ts

---
+++ frontend/src/components/progress/index.ts
@@ -0,0 +1,4 @@
+export { default as ProgressBar } from "./ProgressBar";
+export { default as ProgressCard } from "./ProgressCard";
+export { default as ProgressStats } from "./ProgressStats";
+export { default as ProgressTimeline } from "./ProgressTimeline";
